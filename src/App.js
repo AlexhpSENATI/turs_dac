6f8eb33ac6c2e6d8ff46b593c93fc2429715fcf0
AlexhpSENATI <1510696@senati.pe>
2025-05-06 00:35:37 -0500

    Inicio con navar y carrusel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+import React from "react";
+import SecHero from './components/sec_hero.jsx';
+import Inicio from './components/index.jsx';
+import Contacto from './components/contact.jsx';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* NAVAR Y CARRUSEL */}
+        <Route path="/sec_hero" element={<SecHero />} />
+
+        {/* TODOS LOS JSX */}
+        <Route path="/" element={<Inicio />} />
+        <Route path="/contacto" element={<Contacto />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <SecHero />
+//     </div>
+//     <div className="XD">
+//     <Index />
+//   </div>
+    
+//   );
+// }
+
+// export default App;
